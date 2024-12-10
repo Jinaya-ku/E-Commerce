c1f916e275b1999e99b85a0571d094020e257572
Jinaya-ku <aliuspanji@gmail.com>
2024-12-11 01:34:55 +0700

    memperbaiki eror

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 
 class ProfileController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $user = FacadesAuth::user();
         return view("profile");
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+@foreach ($produks as $item)
 <div class="container py-4">
     <div class="py-5">
         <div class="card" style="width: 20rem;">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5mPq85oI0Zeiqas4yR4PtJ0rtU21pVgOv6A&s"
                 class="card-img-top" alt="gambar tas">
             <div class="card-body">
-                <h5 class="card-title">Tas</h5>
-                <p class="card-text">Tas pria</p>
+                <h5 class="card-title">{{$item->nama}}</h5>
+                <p class="card-text">{{$item->nama}}</p>
                 <div class="d-flex align-items-center justify-content-between">
                     <a href="#" class="btn btn-primary">Add to cart</a>
-                    <span>Rp.200.000, 00</span>
+                    <span>{{$item->harga}}</span>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
+@endforeach
 @endsection
