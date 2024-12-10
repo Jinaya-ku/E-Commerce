cb376c71f5d52bcd3f9fabbcdbb3f6aedd65620e
eksa-arifa <arifamuhammad1536@gmail.com>
2024-12-10 22:50:31 +0700

    Add: Home, login, register page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello world</h1>
+<div class="container py-4">
+    <div class="py-5">
+        <div class="card" style="width: 15rem;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5mPq85oI0Zeiqas4yR4PtJ0rtU21pVgOv6A&s"
+                class="card-img-top" alt="gambar tas">
+            <div class="card-body">
+                <h5 class="card-title">Tas</h5>
+                <p class="card-text">Tas pria</p>
+                <div class="d-flex align-items-center justify-content-between">
+                    <a href="#" class="btn btn-primary">Buy</a>
+                    <span>Rp.200.000, 00</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EcommerceController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('guest')->group(function(){
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::get('register', [AuthController::class, 'register'])->name('register');
 });
 
 // Rute untuk menampilkan daftar produk
